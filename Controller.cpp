bf282183a9c5ea5e7786ff4b2bad7c2b54de3f58
Turgure <toranana_bye@yahoo.co.jp>
2013-11-03 17:38:39 +0900

    introduce jumping

--- Controller.cpp
+++ Controller.cpp
@@ -7,16 +7,44 @@
 void Controller::update(){
 	auto& position = getObject()->getComponentAs<Transform>("Transform");
 
-	if(Input::getInstance().pushing(KEY_INPUT_RIGHT)){
+	if (Input::getInstance().pushing(KEY_INPUT_RIGHT)){
 		*position += Vector2(5, 0);
 	}
-	if(Input::getInstance().pushing(KEY_INPUT_LEFT)){
+	if (Input::getInstance().pushing(KEY_INPUT_LEFT)){
 		*position += Vector2(-5, 0);
 	}
-	if(Input::getInstance().pushing(KEY_INPUT_UP)){
-		*position += Vector2(0, -5);
+	/*
+	if (Input::getInstance().pushing(KEY_INPUT_UP)){
+	*position += Vector2(0, -5);
 	}
-	if(Input::getInstance().pushing(KEY_INPUT_DOWN)){
-		*position += Vector2(0, 5);
+	if (Input::getInstance().pushing(KEY_INPUT_DOWN)){
+	*position += Vector2(0, 5);
+	}
+	*/
+	printfDx("%d", jump_power);
+
+	if (Input::getInstance().pushed(KEY_INPUT_SPACE, false) && status != FALL){
+		status = JUMP;
+	}
+	if (Input::getInstance().pushing(KEY_INPUT_SPACE)){
+		jump_power += 2;
+		jump_power >= 20 ? 20 : jump_power;
+	}
+
+	if (status == JUMP){
+		*position -= Vector2(0, jump_power / (++frame)*2);
+		if (frame >= jump_power){
+			status = FALL;
+			frame = 0;
+		}
+	}
+	else if (status == FALL){
+		*position += Vector2(0, (frame++));
+		if (position->getY() > 480 - 32){
+			position->setY(480 - 32);
+			status = STOP;
+			frame = 0;
+			jump_power = 10;
+		}
 	}
 }
--- Controller.h
+++ Controller.h
@@ -3,11 +3,27 @@
 #include "Component.h"
 
 class Controller : public Component{
+private:
+	enum Status{
+		EMPTY,
+		STOP,
+		WALK,
+		RUN,
+		JUMP,
+		FALL,
+		STATUS_NUM
+	} status;
+
 public:
 	virtual const std::string& getKeyString() const override {
 		static std::string key = "Gamepad";
 		return key;
 	}
 
+	Controller() :status(EMPTY){}
 	void update() override;
+
+private:
+	int frame = 0;
+	int jump_power = 10;
 };
